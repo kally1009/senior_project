18e2024badbba4fc12e66e5155b4a5dad90a7eb6
Kalicia Adams <kaliciaanne@gmail.com>
2024-04-15 22:23:31 -0600

    ideas for more interesting stats and queries

--- stats/main.py
+++ stats/main.py
@@ -28,3 +28,25 @@ for data in x:
 
 
 sys.stdout.flush()
+
+### Queries to write:
+
+# most common mood out of all entries
+# Counts for each mood type
+# Counts for each activity
+# Most common activity
+# overlap: most common mood and activity pairing
+# Last day of best (5) mood
+
+
+# Other ideas:
+# Mood Frequencies
+# Activity Frequencies
+# Activities, their frequency, and average mood score.
+# Activities, their frequency, and the mood’s standard deviation
+# Average mood score difference when an activity is and isn’t present
+#Average mood score with the activity present, without the activity, and the difference (table)
+#Top 5 activity combinations
+#Time series trend
+# The time series’ yearly seasonality
+
